0f433c8236e32a5628ef0710f4e54c8653c282c8
Alex-Pennington <alex.pennington@organicengineer.com>
2026-02-17 18:18:20 -0500

    Show version in home screen hero meta (v23)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Phoenix Nest Training App - Service Worker
 // Cache-first strategy for full offline support
 
-const CACHE_NAME = 'pn-training-v22';
+const CACHE_NAME = 'pn-training-v23';
 const ASSETS = [
   './',
   './index.html',
